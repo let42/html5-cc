c5ebe28562d768c5bccdb8f207eabb109f60e8d1
Xlet42X <xlet42x@gmail.com>
2018-12-30 23:50:02 +0100

    Creato nuova form con i radio buttons

---
+++ src/LeForm/radio.js
@@ -0,0 +1,14 @@
+//Printing values
+function printMe(nome, cognome, numTel, note, studente, sesso, eta){
+    if(studente == "on")
+        studente = "Sì";
+    else if (studente == "off") {
+        studente = "No";
+    }
+    var strinToPrint="<p>Nome: "+ nome + "<br />"
+    + "Cognome: " + cognome + "<br />" +
+    " Numero di Telefono: " + numTel + "<br />" +
+    "Note: " + note + "<br />"+ "Studente: " + studente + "<br />" +
+    "Sesso: " + sesso + "<br />" + "Età: " + eta + "</p>";
+    document.getElementById('printarea').innerHTML= strinToPrint;
+}
